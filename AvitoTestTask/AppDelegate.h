99b16f5ef0627047981ca33ce93ed825850101ba
Aleksey Kolchanov <kolchanov.aleksey@gmail.com>
2015-01-21 22:39:15 +0500

    Initial Commit

---
+++ AvitoTestTask/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AvitoTestTask
+//
+//  Created by Пользователь on 21.01.15.
+//  Copyright (c) 2015 AlKol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AvitoTestTask/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AvitoTestTask
+//
+//  Created by Пользователь on 21.01.15.
+//  Copyright (c) 2015 AlKol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
